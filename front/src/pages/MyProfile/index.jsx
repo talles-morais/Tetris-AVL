044df2d7c9aedc862d78365a05020f51a8c5a7d1
Talles <talves2503@gmail.com>
2023-12-08 17:25:37 -0300

    back to game button added

--- front/src/pages/MyProfile/index.jsx
+++ front/src/pages/MyProfile/index.jsx
@@ -1,17 +1,44 @@
 import Button from '../../components/Button';
 import FormField from '../../components/FormField';
+import { Link } from 'react-router-dom';
+import goBackIcon from '../../assets/icons/goBack.png';
 import './MyProfile.css';
 
 export default function MyProfile() {
     return (
         <main className="main">
             <section className="myProfile">
-                <h1>Meu Perfil</h1>
+                <div className="titleContainer">
+                    <Link to="/game">
+                        <img src={goBackIcon} id="goBackBtn" />
+                    </Link>
+                    <h1>Meu Perfil</h1>
+                </div>
                 <form action="" className="changeData">
-                    <FormField label="Email:" htmlFor="email" type="text" placeholder="Digite seu email..."/>
-                    <FormField label="Nickname:" htmlFor="nickname" type="text" placeholder="Digite seu apelido..."/>
-                    <FormField label="Senha:" htmlFor="password" type="text" placeholder="Digite uma nova senha..."/>
-                    <FormField label="Confirme sua senha:" htmlFor="passwordConfirm" type="text" placeholder="Digite sua senha novamente..."/>
+                    <FormField
+                        label="Email:"
+                        htmlFor="email"
+                        type="text"
+                        placeholder="Digite seu email..."
+                    />
+                    <FormField
+                        label="Nickname:"
+                        htmlFor="nickname"
+                        type="text"
+                        placeholder="Digite seu apelido..."
+                    />
+                    <FormField
+                        label="Senha:"
+                        htmlFor="password"
+                        type="text"
+                        placeholder="Digite uma nova senha..."
+                    />
+                    <FormField
+                        label="Confirme sua senha:"
+                        htmlFor="passwordConfirm"
+                        type="text"
+                        placeholder="Digite sua senha novamente..."
+                    />
 
                     <Button text="Alterar dados" type="submit" />
                 </form>
